964bee18152983e4e42a3573e39041272b170d2c
xfy <i@rua.plus>
2024-07-30 11:51:28 +0800

    feat(state): add zustand

---
+++ src/store/ui.ts
@@ -0,0 +1,25 @@
+import { StateCreator } from 'zustand';
+import { immer } from 'zustand/middleware/immer';
+
+export type MenuType = 'global' | 'node' | 'routing';
+export type NodeDrawerType = 'editor' | 'share';
+export type RoutingDrawerType = 'editor' | 'add';
+
+export interface UI {
+  // content menu on right click
+  showMenu: string | null;
+}
+export interface UIAction {}
+
+export type UISlice = UI & UIAction;
+
+const createUISlice: StateCreator<
+  UISlice,
+  [],
+  [['zustand/immer', never]],
+  UISlice
+> = immer<UISlice>((set) => ({
+  showMenu: null,
+}));
+
+export default createUISlice;
