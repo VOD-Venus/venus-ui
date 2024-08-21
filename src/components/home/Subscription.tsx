fdfe7b7faf700baff3ee4affe071c651f0bc50e3
xfy <xfyxfy@proton.me>
2024-08-21 16:42:29 +0800

    feat(ui): add subscription card

---
+++ src/components/home/Subscription.tsx
@@ -0,0 +1,13 @@
+import { Card, CardBody } from '@nextui-org/card';
+
+const Subscription = () => {
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      <Card className="max-w-80 justify-self-center w-full">
+        <CardBody>test</CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default Subscription;
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,16 @@
 import { Tab, Tabs } from '@nextui-org/tabs';
 import clsx from 'clsx';
+import Subscription from 'components/home/Subscription';
 import Title from 'components/pages/Title';
+import { useState } from 'react';
 
 const Index = () => {
+  const Children = {
+    subscription: <Subscription />,
+    nodes: <>Nodes</>,
+  };
+  const [current, setCurrent] = useState();
+
   return (
     <>
       <div className={clsx('flex flex-col h-full')}>
@@ -12,10 +20,16 @@ const Index = () => {
           </div>
         </div>
 
-        <Tabs variant="underlined" size="lg">
-          <Tab title="Subscription" />
-          <Tab title="Nodes" />
+        <Tabs
+          variant="underlined"
+          size="lg"
+          onSelectionChange={(key) => setCurrent(key)}
+        >
+          <Tab key="subscription" title="Subscription" />
+          <Tab key="nodes" title="Nodes" />
         </Tabs>
+
+        <div className="p-4">{Children[current]}</div>
       </div>
     </>
   );
